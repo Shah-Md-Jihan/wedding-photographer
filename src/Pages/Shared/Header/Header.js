2d721b5fb8740d57b44e522562dfe51395db10f5
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-11-08 14:45:55 +0600

    nav bar updated

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,24 +2,29 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
 import { Link } from 'react-router-dom';
 
 
 const Header = () => {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="ms-auto">
-                    <Nav className='me-4'>
-                        <Link to={"/"} className="text-decoration-none text-white">Home</Link>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav className='me-4'>
+                            <Link to={'/'} className="text-decoration-none text-white">Home</Link>
+                        </Nav>
+                        <Nav className='me-4'>
+                            <Link to={'/login'} className="text-decoration-none text-white">Login</Link>
+                        </Nav>
                     </Nav>
-                    <Nav className='me-4'>
-                        <Link to={"/login"} className="text-decoration-none text-white">Login</Link>
-                    </Nav>
-                </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
+
 
     );
 };
